ac301849f1e5ea61e15a9dd8ef6c4280c8fa4b9d
Wesley <wmixgamer@gmail.com>
2021-09-09 17:05:17 -0300

    Interfaces adicionadas

---
+++ senai.inlock.webApi/senai.inlock.webApi/Interfaces/IJogoRepository.cs
@@ -0,0 +1,24 @@
+﻿using senai.inlock.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    interface IJogoRepository
+    {
+
+            List<JogoDomain> ListarTodos();
+
+            JogoDomain BuscarPorId(int idJogo);
+
+            void Cadastrar(JogoDomain novoJogo);
+
+            void AtualizarIdCorpo(JogoDomain jogoAtualizado);
+
+            void Deletar(int idjogo);
+
+       
+    }
+}
---
+++ senai.inlock.webApi/senai.inlock.webApi/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,22 @@
+﻿using senai.inlock.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Interfaces
+{
+    interface IUsuarioRepository
+    {
+
+        List<UsuarioDomain> ListarTodos();
+
+        UsuarioDomain BuscarPorId(int idUsuario);
+
+        void Cadastrar(UsuarioDomain novoUsuario);
+
+        void AtualizarIdCorpo(UsuarioDomain usuarioAtualizado);
+
+        void Deletar(int idUsuario);
+    }
+}
