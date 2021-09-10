265d564aff9f043e82dfcf1470eb306b37a6b320
VITOR GONÇALVES <vitor.goncalves5@portalsesisp.org.br>
2021-09-10 15:51:45 -0300

    BackupControllerUsuarios

---
+++ senai.inlock.webApi/senai.inlock.webApi/Controllers/UsuariosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.inlock.webApi.Domains;
+using senai.inlock.webApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.inlock.webApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(UsuarioDomain login)
+        {
+            UsuarioDomain usuarioBuscado = _usuarioRepository.BuscarPorEmailSenha(login.email, login.senha);
+
+            if (usuarioBuscado == null)
+                return NotFound("E-mail ou senha inválidos!");
+
+            return Ok(usuarioBuscado);
+        }
+    }
+}
